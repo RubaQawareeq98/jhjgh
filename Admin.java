2624327026846da7c5d60bdcdb0cd7f6f4163d3b
ruba <ruba@gmail.com>
2023-10-19 22:10:48 +0300

    First commit

---
+++ Admin.java
@@ -0,0 +1,16 @@
+package entities;
+
+public class Admin {
+    private String email;
+
+    public Admin() {
+        //don't use it
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String name) {
+        this.email = name;
+    }
+
+}
